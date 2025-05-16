7fe821b23dfa88609683e80ca551b3330582371c
AshenLanarolle <ashenkavishkao97@gmaol.com>
2025-05-16 15:22:38 +0530

    Requirments added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Genarative AI Project',
+    version = '0.0.0',
+    author = 'Ashen',
+    author_email='ashenkavishkao97@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
